f696b2819b34092d3f2502c84c220f00bae3ff32
Alexander Fedyashov <af@e42.guru>
2016-06-13 19:05:48 +0300

    Parser code #2

--- src/Parser.php
+++ src/Parser.php
@@ -1,46 +1,94 @@
 <?php
+/**
+ * TLDDatabase: Abstraction for Public Suffix List in PHP.
+ *
+ * @link      https://github.com/layershifter/TLDDatabase
+ *
+ * @copyright Copyright (c) 2016, Alexander Fedyashov
+ * @license   https://raw.githubusercontent.com/layershifter/TLDDatabase/master/LICENSE Apache 2.0 License
+ */
 
-namespace Layershifter\TLDDatabase;
+namespace LayerShifter\TLDDatabase;
 
-use Layershifter\TLDDatabase\Http\Curl;
+use LayerShifter\TLDExtract\Exceptions\ParserException;
+use LayerShifter\TLDSupport\Helpers\Str;
 
-class Parser
+/**
+ * Class for parsing input of lines from Public Suffix List.
+ */
+final class Parser
 {
 
-    const PUBLIC_SUFFIX_LIST_URL = '';
+    /**
+     * @const string String which shows that the line is a comment.
+     */
+    const COMMENT_STRING_START = '//';
+    /**
+     * @const string Commentary string which shows that beginning of private domains list.
+     */
+    const PRIVATE_DOMAINS_STRING = '// ===BEGIN PRIVATE DOMAINS===';
 
-    private $isIccanSuffix = true;
-    private $httpAdapter;
-    private $outputFileName;
-    private $suffixes = [];
+    /**
+     * @var bool Flags which indicates what type of domain is currently parsed.
+     */
+    private $isICCANSuffix = true;
+    /**
+     * @var array|string[] Input array of lines from Public Suffix List.
+     */
+    private $lines = [];
 
     /**
      * Parser constructor.
      *
-     * @param string $outputFileName
-     * @param string $httpAdapter
+     * @param array|string[] $lines Array of lines from Public Suffix List
      */
-    public function __construct($outputFileName, $httpAdapter = null)
+    public function __construct($lines)
     {
-        $this->outputFileName = $outputFileName;
-
-        if (null === $httpAdapter) {
-            $this->httpAdapter = new Curl();
-
-            return;
-        }
-
-        if (!class_exists($httpAdapter)) {
-            // TODO: Exception
-        }
-
-        // TODO: Check inheritance of interface
+        $this->lines = $lines;
     }
 
+    /**
+     * Method that parses submitted strings and returns array from valid suffixes.
+     *
+     * Parser features the following rules apply:
+     * - the list is a set of rules, with one rule per line;
+     * - each line is only read up to the first whitespace; entire lines can also be commented using //;
+     * - each line which is not entirely whitespace or begins with a comment contains a rule;
+     *
+     * @see https://publicsuffix.org/list/
+     *
+     * @return array|string[]
+     *
+     * @throws ParserException
+     */
     public function parse()
     {
-        foreach ($this->httpAdapter->get() as $line) {
+        $suffixes = [];
 
+        foreach ($this->lines as $line) {
+            if (Str::startsWith($line, self::PRIVATE_DOMAINS_STRING)) {
+                $this->isICCANSuffix = false;
+
+                continue;
+            }
+
+            if (Str::startsWith($line, self::COMMENT_STRING_START)) {
+                continue;
+            }
+
+            $line = explode(' ', trim($line))[0];
+
+            if (Str::length($line) === 0) {
+                continue;
+            }
+
+            $suffixes[] = $line;
         }
+
+        if (count($suffixes) === 0) {
+            throw new ParserException('Input array of lines does not have any valid suffix, check input');
+        }
+
+        return $suffixes;
     }
 }
