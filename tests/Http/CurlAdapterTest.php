77dbf29f39c834e3708f66374417f06c39fdc9ca
Alexander Fedyashov <af@e42.guru>
2016-06-16 17:07:17 +0300

    curl test #5

--- tests/Http/CurlAdapterTest.php
+++ tests/Http/CurlAdapterTest.php
@@ -8,26 +8,27 @@
  * @license   https://raw.githubusercontent.com/layershifter/TLDDatabase/master/LICENSE Apache 2.0 License
  */
 
-namespace Layershifter\TLDDatabase\Tests\Http;
+namespace LayerShifter\TLDDatabase\Tests\Http;
 
-use Layershifter\TLDDatabase\Exceptions\HttpException;
-use Layershifter\TLDDatabase\Http\AdapterInterface;
-use Layershifter\TLDDatabase\Http\CurlAdapter;
+use LayerShifter\TLDDatabase\Exceptions\HttpException;
+use LayerShifter\TLDDatabase\Http\AdapterInterface;
+use LayerShifter\TLDDatabase\Http\CurlAdapter;
 
 /**
- * Class CurlAdapterTest
- * @package Layerschifter\TLDDatabase\Tests\Http
+ * Test for Http\CurlAdapter class.
  */
 class CurlAdapterTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
-     * @var AdapterInterface
+     * @var AdapterInterface Object with CurlAdapter
      */
     private $adapter;
 
     /**
+     * Bootstrap method.
      *
+     * @return void
      */
     protected function setUp()
     {
@@ -36,13 +37,49 @@ class CurlAdapterTest extends \PHPUnit_Framework_TestCase
         parent::setUp();
     }
 
+    /**
+     * Test interface implementing.
+     *
+     * @return void
+     */
+    public function testImplements()
+    {
+        self::assertInstanceOf(AdapterInterface::class, new CurlAdapter());
+    }
 
     /**
+     * Test for failed response with unresolved domain.
      *
+     * @return void
      */
-    public function getTest()
+    public function testGetDomainException()
     {
-        $this->setExpectedException(HttpException::class);
+        $this->setExpectedException(HttpException::class, 'Get cURL error while fetching PSL file:');
         $this->adapter->get('http://google.google');
+    }
+
+    /**
+     * Test for failed response with code.
+     *
+     * @return void
+     */
+    public function testGetCodeException()
+    {
+        $this->setExpectedException(HttpException::class, 'Get invalid HTTP');
+        $this->adapter->get('http://google.com/404.html');
+    }
+
+    /**
+     * Test for valid response.
+     *
+     * @return void
+     */
+    public function testGet()
+    {
+        $validResponse = $this->adapter->get('http://www.google.com/robots.txt');
+
+        self::assertInternalType('array', $validResponse);
+        self::assertGreaterThan(0, count($validResponse));
+        self::assertArrayHasKey(0, $validResponse);
     }
 }
